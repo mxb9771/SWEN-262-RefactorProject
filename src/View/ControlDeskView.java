faf4b655320b23a7d8981e0434cf08bab0d459af
dajanio <oid2260@rit.edu>
2019-04-21 20:52:54 -0400

    Broke up ControlDeskView

--- src/View/ControlDeskView.java
+++ src/View/ControlDeskView.java
@@ -45,16 +45,46 @@ public class ControlDeskView implements ActionListener, ControlDeskObserver {
 
 		this.controlDesk = controlDesk;
 		this.maxMembers = maxMembers;
-		int numLanes = controlDesk.getNumLanes();
+		createWindow();
 
+		win.show();
+
+	}
+
+
+	public void createWindow(){
 		win = new JFrame("Control Desk");
+
 		win.getContentPane().setLayout(new BorderLayout());
 		((JPanel) win.getContentPane()).setOpaque(false);
 
+		JPanel colPanel = createColPanel();
+
+		win.getContentPane().add("Center", colPanel);
+
+		win.pack();
+
+		/* Close program when this window closes */
+		win.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+
+		// Center Window on Screen
+		Dimension screenSize = (Toolkit.getDefaultToolkit()).getScreenSize();
+		win.setLocation(
+				((screenSize.width) / 2) - ((win.getSize().width) / 2),
+				((screenSize.height) / 2) - ((win.getSize().height) / 2));
+
+
+	}
+
+	public JPanel createColPanel(){
+		int numLanes = controlDesk.getNumLanes();
 		JPanel colPanel = new JPanel();
 		colPanel.setLayout(new BorderLayout());
 
-		// Controls Panel
 		JPanel controlsPanel = new JPanel();
 		controlsPanel.setLayout(new GridLayout(3, 1));
 		controlsPanel.setBorder(new TitledBorder("Controls"));
@@ -71,16 +101,7 @@ public class ControlDeskView implements ActionListener, ControlDeskObserver {
 		assignPanel.setLayout(new FlowLayout());
 		assign.addActionListener(this);
 		assignPanel.add(assign);
-//		controlsPanel.add(assignPanel);
 
-		finished = new JButton("Finished");
-		JPanel finishedPanel = new JPanel();
-		finishedPanel.setLayout(new FlowLayout());
-		finished.addActionListener(this);
-		finishedPanel.add(finished);
-		controlsPanel.add(finishedPanel);
-
-		// src.Model.Lane Status Panel
 		JPanel laneStatusPanel = new JPanel();
 		laneStatusPanel.setLayout(new GridLayout(numLanes, 1));
 		laneStatusPanel.setBorder(new TitledBorder("Lane Status"));
@@ -98,7 +119,6 @@ public class ControlDeskView implements ActionListener, ControlDeskObserver {
 			laneStatusPanel.add(lanePanel);
 		}
 
-		// src.Model.Party src.Model.Queue Panel
 		JPanel partyPanel = new JPanel();
 		partyPanel.setLayout(new FlowLayout());
 		partyPanel.setBorder(new TitledBorder("Party Queue"));
@@ -111,34 +131,17 @@ public class ControlDeskView implements ActionListener, ControlDeskObserver {
 		partyList.setVisibleRowCount(10);
 		JScrollPane partyPane = new JScrollPane(partyList);
 		partyPane.setVerticalScrollBarPolicy(
-			JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		partyPanel.add(partyPane);
-		//		partyPanel.add(partyList);
 
-		// Clean up main panel
 		colPanel.add(controlsPanel, "East");
 		colPanel.add(laneStatusPanel, "Center");
 		colPanel.add(partyPanel, "West");
 
-		win.getContentPane().add("Center", colPanel);
-
-		win.pack();
-
-		/* Close program when this window closes */
-		win.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e) {
-				System.exit(0);
-			}
-		});
-
-		// Center Window on Screen
-		Dimension screenSize = (Toolkit.getDefaultToolkit()).getScreenSize();
-		win.setLocation(
-			((screenSize.width) / 2) - ((win.getSize().width) / 2),
-			((screenSize.height) / 2) - ((win.getSize().height) / 2));
-		win.show();
-
+		return colPanel;
 	}
+
+
 
 	/**
 	 * Handler for actionEvents
